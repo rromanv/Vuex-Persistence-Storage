aee514e3441d38c4eae6462276fa987d9a1c6232
Rodolfo Roman <rromanve@southtexascollege.edu>
2020-02-20 16:39:14 -0600

    example

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+
+const ls = new SecureLS({ isCompression: false });
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    count: 0,
+    string: ""
+  },
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: key => ls.remove(key)
+      }
+    })
+  ],
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--,
+    myString: (state, value) =>
+      value ? (state.string = value) : (state.string = "")
+  }
 });
